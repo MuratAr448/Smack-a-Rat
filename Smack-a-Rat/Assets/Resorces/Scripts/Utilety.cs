bf5e0037477e118f828796b041964f94596f7844
MuratAr448 <144346044+MuratAr448@users.noreply.github.com>
2025-09-09 14:19:28 +0200

    TMP && Utilety

---
+++ Smack-a-Rat/Assets/Resorces/Scripts/Utilety.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class Utilety : MonoBehaviour
+{
+    public GameObject start;
+    public GameObject leaderBoard;
+    public GameObject startName;
+    public void ToStart()
+    {
+        start.SetActive(true);
+        startName.SetActive(false);
+        leaderBoard.SetActive(false);
+    }
+    public void ToLeaderboard()
+    {
+        start.SetActive(false);
+        startName.SetActive(false);
+        leaderBoard.SetActive(true);
+    }
+    public void ToStartName()
+    {
+        start.SetActive(false);
+        startName.SetActive(true);
+        leaderBoard.SetActive(false);
+    }
+    public void ToGame()
+    {
+
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
